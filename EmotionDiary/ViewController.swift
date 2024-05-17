a8f3843789ef070b5fe6cf73b0673007f5516fb6
jeongyun-kim <kkanyo04@naver.com>
2024-05-17 18:49:19 +0900

    감정 UI 구성

--- EmotionDiary/ViewController.swift
+++ EmotionDiary/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var emotionImageViewCollection: [UIImageView]!
+    @IBOutlet var emotionLabelCollection: [UILabel]!
+    
 
+    let emotions: [String] = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "피곤해", "씁쓸해", "이상해", "우울해"]
+    var emotionsDict: [String:Int] = [:]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for i in (0..<emotionLabelCollection.count) {
+            configureEmotions(imageView: emotionImageViewCollection[i], imageName: "slime\(i+1)", label: emotionLabelCollection[i], emotion: emotions[i])
+        }
     }
 
-
+    func configureEmotions(imageView: UIImageView, imageName: String, label: UILabel, emotion: String) {
+        emotionsDict[emotion] = 0
+        
+        imageView.image = UIImage(named: imageName)
+        imageView.contentMode = .scaleAspectFit
+        label.textAlignment = .center
+        label.text = "\(emotion) 0"
+    }
 }
 
