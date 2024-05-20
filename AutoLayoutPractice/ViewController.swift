c518a64909048481ad9c9f986a7610ef36250ea7
jeongyun-kim <kkanyo04@naver.com>
2024-05-21 00:15:37 +0900

    카카오톡 프로필 화면 디자인

--- AutoLayoutPractice/ViewController.swift
+++ AutoLayoutPractice/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var profileNameLabel: UILabel!
+    @IBOutlet var imageViews: [UIImageView]!
+    @IBOutlet var labels: [UILabel]!
+    
+    let imageNames: [String] = ["xmark", "gift.circle", "circle.grid.3x3.circle", "gearshape.circle", "message.fill", "pencil", "quote.closing"]
+    let labelTexts: [String] = ["나와의 채팅", "프로필 편집", "카카오스토리"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
+    func setupUI() {
+        backgroundImageView.image = UIImage(named: "kakaotalk_background")
+        backgroundImageView.contentMode = .scaleAspectFill
+        
+        for i in (0..<buttons.count) {
+            let btn = buttons[i]
+            btn.setTitle("", for: .normal)
+            btn.setImage(UIImage(systemName: imageNames[i]), for: .normal)
+            btn.tintColor = .white
+        }
+        
+        for i in (0..<imageViews.count) {
+            let imageView = imageViews[i]
+            let title = labels[i]
+            imageView.image = UIImage(systemName: imageNames[i+4])
+            imageView.tintColor = .white
+            imageView.contentMode = .scaleAspectFit
+            title.text = labelTexts[i]
+            title.textAlignment = .center
+            title.textColor = .white
+            title.font = UIFont.systemFont(ofSize: 12)
+        }
+        profileImageView.image = UIImage(named: "profile")
+        profileImageView.contentMode = .scaleAspectFill
+        profileImageView.layer.cornerRadius = profileImageView.frame.width / 2 - 8
+        profileNameLabel.font = UIFont.systemFont(ofSize: 14)
+        profileNameLabel.textColor = .white
+        
+    }
 
 }
 
