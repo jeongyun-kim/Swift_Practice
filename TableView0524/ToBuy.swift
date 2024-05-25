3be5e91e45b4d42c162a194d511a117b7280d85c
jeongyun-kim <kkanyo04@naver.com>
2024-05-25 15:08:49 +0900

    TableView로 쇼핑 목록 리스트 구현하기

---
+++ TableView0524/ToBuy.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  TableView0524
+//
+//  Created by 김정윤 on 5/24/24.
+//
+
+import Foundation
+
+struct ToBuy {
+    let toBuy: String
+    var complete: Bool
+    var importance: Bool
+}
---
+++ TableView0524/ToBuyCell.swift
@@ -0,0 +1,16 @@
+//
+//  TodoCell.swift
+//  TableView0524
+//
+//  Created by 김정윤 on 5/24/24.
+//
+
+import UIKit
+
+class ToBuyCell: UITableViewCell {
+    @IBOutlet var cellView: UIView!
+    @IBOutlet var completeBtn: UIButton!
+    @IBOutlet var importanceBtn: UIButton!
+    @IBOutlet var toBuyLabel: UILabel!
+    
+}
