579c70b4b98375e43f359b3cf2d32614cc20ddeb
jeongyun-kim <kkanyo04@naver.com>
2024-10-23 20:46:29 +0900

    init: TCA 라이브러리 설치

---
+++ TCA_Practice/TCA_Practice/TCA_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PracticeApp.swift
+//  TCA_Practice
+//
+//  Created by 김정윤 on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCA_Practice/TCA_PracticeTests/TCA_PracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PracticeTests.swift
+//  TCA_PracticeTests
+//
+//  Created by 김정윤 on 10/23/24.
+//
+
+import Testing
+@testable import TCA_Practice
+
+struct TCA_PracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
