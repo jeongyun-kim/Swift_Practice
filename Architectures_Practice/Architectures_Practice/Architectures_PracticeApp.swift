bbb0716349aa0e6f56756e10594f738c97914876
jeongyun-kim <kkanyo04@naver.com>
2024-10-24 23:30:17 +0900

    init

---
+++ Architectures_Practice/Architectures_Practice/Architectures_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Architectures_PracticeApp.swift
+//  Architectures_Practice
+//
+//  Created by 김정윤 on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Architectures_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Architectures_Practice/Architectures_PracticeTests/Architectures_PracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Architectures_PracticeTests.swift
+//  Architectures_PracticeTests
+//
+//  Created by 김정윤 on 10/24/24.
+//
+
+import Testing
+@testable import Architectures_Practice
+
+struct Architectures_PracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
