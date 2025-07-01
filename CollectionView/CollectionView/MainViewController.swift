b56360cb0edcea8cea6854ef199b498a238f0bc2
jeongyun-kim <kkanyo04@naver.com>
2025-07-01 15:25:14 +0900

    init

---
+++ CollectionView/CollectionView/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  CollectionView
+//
+//  Created by 김정윤 on 7/1/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
